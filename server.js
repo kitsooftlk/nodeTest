97c69bcceeda434307657754d0ae818797c42a9b
kitsooftlk <32268932+kitsooftlk@users.noreply.github.com>
2017-09-26 00:05:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-/* Hello World! program in Node.js */
+/*/* Hello World! program in Node.js */
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//const server = http.createServer((req, res) => {
-//  res.statusCode = 200;
-//  //res.setHeader('Content-Type', 'text/plain');
-//  res.end('<title> Hello World\n </title>');
-//});
-   
-//server.listen(port);
-console.log("END");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  res.end('<title> Hello World\n </title>');
+});
+ 
+server.listen(port);
+console.log("END");*/
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
