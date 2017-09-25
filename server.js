aec67ef5c1090ec0f08c594a93e0da0f3d659236
kitsooftlk <32268932+kitsooftlk@users.noreply.github.com>
2017-09-26 00:14:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 31000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
